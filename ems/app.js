d581cf0a909828d833d30e2af012c442eb5f02e4
Travis-Rosen <tmrosen@my365.bellevue.edu>
2021-05-09 22:42:37 -0500

    Adding week-8 part 2

--- ems/app.js
+++ ems/app.js
@@ -3,11 +3,47 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var logger = require('morgan');
+var helmet = require('helmet');
+var Employee = require('.models/employee');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var csrf = require ('csurf');
+
 
 var app = express();
+var mongoose = require("mongoose");
+var csrfProtection = csrf({cookie:true});
+var Employee = require('./models/employee');
+
+var mongoDB = 'mongodb+srv://tmrosen:tmrosen@buwebdev-cluster-1.azoni.mongodb.net/test';
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function(){
+    console.log("Application connected to mLab MongoDB instance.");
+});
+
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('short'));
+app.use(helmet.xssFilter());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.use(cookieParser());
+app.use(csrfProtection);
+
+app.use(function(req, res, next){
+    var token = req.csrfToken();
+    res.cookie('XSRF-TOKEN', token);
+    res.locals.csrfToken = token;
+    next();
+})
 
 app.get('/', function(req, res){
     res.render('index', {
@@ -15,6 +51,24 @@ app.get('/', function(req, res){
     });
 });
 
+app.get('/new', function(req, res){
+    res.render('new', {
+        title: "New",
+        message: "New Entry."
+        
+    });
+});
+
+app.post('/process', function(req, res){
+    console.log(request.body.txtName);
+    response.redirect("/");
+});
+
+let Employee = new Employee({
+    firstName: "Emily",
+    lastName: "Braun"
+});
+    
 //Creating Server
 http.createServer(app).listen(8080, function() {
     console.log("Application has started on port 8080");
--- ems/models/employees.js
+++ ems/models/employees.js
@@ -3,45 +3,13 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 //Defining the employeeSchema. (First and Last Name)
-var employeeSchema = new Schema({
-    firstName:String,
-    lastName: String
+var EmployeeSchema = new Schema({
+    firstName: { type: String, required: true, unique: true},
+    lastName: {type: String, required: true, unique: true},
 });
 
 //Defining the model
-var Employee = mongoose.model("Employee", employeeSchema);
+var Employee = mongoose.model("Employee", EmployeeSchema);
 
 //Exporting module.
 module.exports = Employee;
-
-//Require statements for express, http, mongoose, and morgan.
-var express = require("express");
-var http = require("http");
-var mongoose = require("mongoose");
-var logger = require("morgan");
-var Employee = require("./models/employees");
-
-//Connection to mongoDB
-var mongoDB = 'mongodb+srv://tmrosen:tmrosen@buwebdev-cluster-1.azoni.mongodb.net/test';
-mongoose.connect(mongoDB, {
-    useMongoClient: true
-});
-//Message and errors
-mongoose.Promise = global.Promise;
-db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-    console.log("Application connected to mLab MongoDB instance.");
-});
-//Defining express app and logger.
-var app = express();
-app.use(logger('short'));
-//Defining new Employee.
-var Employee = new Employee({
-    firstName: "Emily",
-    lastName: "Braun"
-});
-//Creating server.
-http.createServer(app).listen(8080, function(){
-    console.log("Applications connected and running on port 8080.")
-});
